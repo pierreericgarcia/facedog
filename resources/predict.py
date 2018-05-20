9bc3b25baf5d84cd4e21df60a080fd41945d5a23
Pierre-Eric Garcia <darkaosis@gmail.com>
2018-05-20 15:16:48 +0200

    img data

--- resources/predict.py
+++ resources/predict.py
@@ -3,21 +3,26 @@ from utils.dog_app import dog_app
 import werkzeug
 from PIL import Image
 import io
+import requests
 
 
 class Prediction(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        'url', 
-        type=str, 
-        required=True, 
-        help="I need an image url."
+        'url',
+        type=str,
+        required=True,
+        help="Need an image url to analyze."
     )
 
     def post(self):
         data = Prediction.parser.parse_args()
-        prediction = dog_app(data["url"])
+        url = data['url']
+        img_data = requests.get(url).content
+        print(img_data)
+        image = Image.open(io.BytesIO(img_data)).convert("RGB")
+        prediction = dog_app(image)
         return prediction
 
     def get(self):
-        return {'success': 'Endpoint working'}
+        return { 'success': 'Endpoint working' }
